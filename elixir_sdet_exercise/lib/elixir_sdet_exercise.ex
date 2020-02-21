dc0b0b4f0f80b1cce3b8268eb0274c3659cb6913
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-02-20 21:13:24 -0500

    Compiling FacebookHelper

--- elixir_sdet_exercise/lib/elixir_sdet_exercise.ex
+++ elixir_sdet_exercise/lib/elixir_sdet_exercise.ex
@@ -10,29 +10,93 @@ defmodule FacebookHelper do
 
   use Hound.Helpers
 
-  @dummyuser %{first: "Bob",
-         last: "Blaine",
-         phone: "555-333-1234",
-         email: "my_email@email.com",
-         password: "B6$_tal2Hq5",
-         birthday: "Jan 01 1990",
+  @validdata %{first: {"type", "firstname", "Bob"},
+         last: {"type", "lastname", "Blaine"},
+         email: {"type", "reg_email__", "my_email@email.com"},
+         email_confirmation: {"type", "reg_email_confirmation__", "my_email@email.com"},
+         password: {"password", "reg_passwd__", "B6$_tal2Hq5"},
+         month: {"select", "month", "1"},
+         day: {"select", "day", "1"},
+         year: {"select", "year", "1990"},
         }
 
-  def isLegacyPage do
+  @confirmdata %{email: {"type", "reg_email_confirmation__", "y_email@email.com"},
+                }
+
+  @invaliddata %{email: {"type", "reg_email__", "my_email@email"},
+         password: {"password", "reg_passwd__", "mypassword"},
+         month: {"select", "month", "month"},
+         day: {"select", "day", "-1"},
+         year: {"select", "year", "sixties"},
+        }
+
+  def is_legacy_page do
 
   end
 
-  def launchFacebook do
+  def launch_facebook do
     navigate_to("http://facebook.com")
-    #assert page_title()=="Facebook - Log In or Sign Up"
+  end
+
+  def fb_type_valid_field(my_field) do
+    fb_type_field(Map.get(@validdata, my_field))
+    # ensure confirmation fields are filled
+    #if my_field==:email do
+      #fb_type_field_impl(Map.get(@confirmdata, my_field))
+    #end
+  end
+
+  def fb_type_field(field_tuple) do
+    element_name = find_element(:name, elem(field_tuple, 1))
+    fill_field(element_name, elem(field_tuple, 2))
+  end
+
+  def select_drop_down(drop_down, option) do
+    find_element(:css, "##{drop_down} option[value='#{option}']") |> click()
+  end
+
+  def fb_clear_field(my_field) do
+    element_name = find_element(:name, elem(Map.get(@validdata, my_field), 1))
+    clear_field(element_name)
+  end
+
+  def fb_submit_form do
+    sign_up_button = find_element(:name, "websubmit")
+    click(sign_up_button)
   end
 
   def register_no_first do
-    launchFacebook()
+    launch_facebook()
 
     first_name_element = find_element(:name, "firstname")
     fill_field(first_name_element, @dummyuser.first)
     submit_element(first_name_element)
+  end
+
+  def register_valid_user do
+    register_loop()
+    #register_full_manual()
+  end
+
+  def register_loop do
+    Enum.each  @validdata,  fn {k, v} ->
+      cond do
+        elem(v, 0)=="type" ->
+          fb_type_valid_field(k)
+        elem(v, 0)=="select" ->
+          select_drop_down(k, elem(v, 2))
+        true ->
+          IO.puts "No match"
+      end
+    end
+  end
+
+  def register_full_manual do
+    fb_type_field(:first)
+    fb_type_field(:last)
+    fb_type_field(:email)
+    fb_type_field(:password)
+    select_drop_down(:month, "1")
   end
 
 end
--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -2,76 +2,39 @@ defmodule ElixirSdetExerciseTest do
   # Import helpers
   use Hound.Helpers
   use ExUnit.Case
-  #use FacebookHelper
+  import FacebookHelper
 
   # Start hound session and destroy when tests are run
   hound_session()
 
-  @validdata %{first: {"firstname", "Bob"},
-         last: {"lastname", "Blaine"},
-         phone: {"reg_email__", "555-333-1234"},
-         email: {"reg_email__", "my_email@email.com"},
-         password: {"reg_passwd__", "B6$_tal2Hq5"},
-         birthday_month: {"birthday_month", "Jan"},
-         birthday_day: {"birthday_day", "01"},
-         birthday_year: {"birthday_year", "1990"},
-        }
-
-  def navigateToFacebook do
-    navigate_to("http://facebook.com")
+  def navigate_to_facebook do
+    launch_facebook()
     assert page_title()=="Facebook - Log In or Sign Up"
-  end
-
-  def submitFacebookForm do
-    sign_up_button = find_element(:name, "websubmit")
-    click(sign_up_button)
-  end
-
-  def clear_element_field(element_name) do
-    element = find_element(:name, element_name)
-    clear_field(element)
-  end
-
-  def register_valid_user do
-    register_loop(0)
-  end
-
-  def register_loop(n) do
-    if n < 8 do
-      fielddata = elem(@validdata, n)
-      element_name = elem(fielddata, 0)
-      element_value = elem(fielddata, 1)
-
-      # fill single element with valid data
-      first_name_element = find_element(:name, element_name)
-      fill_field(first_name_element, element_value)
-
-      register_loop(n+1)
-    end
   end
 
   @tag :smoke
   test "submit blank fields" do
-    navigateToFacebook()
+    navigate_to_facebook()
 
-    first_name_element = find_element(:name, elem(@validdata.first, 0))
-    fill_field(first_name_element, "FirstName")
+    fb_type_valid_field(:first)
     #submit_element(first_name_element)
-    submitFacebookForm()
+    fb_submit_form()
     #Process.sleep(5000)
   end
 
   test "without first name" do
-    navigateToFacebook()
+    navigate_to_facebook()
+    register_valid_user()
+    fb_clear_field(:first)
+  end
 
-    #register_valid_user()
+  test "without last name" do
+    navigate_to_facebook()
+    register_valid_user()
+    fb_clear_field(:last)
+  end
 
-    #first_name_element = find_element(:name, "firstname")
-    #fill_field(first_name_element, "FirstName")
-    #Process.sleep(5000)
-    clear_element_field(elem(@validdata.first, 0))
-    #submit_element(first_name_element)
+  test "no email confirmation" do
 
-    #Process.sleep(5000)
   end
 end
