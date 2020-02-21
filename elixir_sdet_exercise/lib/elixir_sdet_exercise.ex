dd560d14007e220fffecdb280794967f58e3a6b8
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-02-21 00:23:12 -0500

    assertions in place

--- elixir_sdet_exercise/lib/elixir_sdet_exercise.ex
+++ elixir_sdet_exercise/lib/elixir_sdet_exercise.ex
@@ -1,11 +1,3 @@
-defmodule ElixirSdetExercise do
-  def hello do
-    :world
-    val = "Hello World!"
-    IO.puts "#{val}"
-  end
-end
-
 defmodule FacebookHelper do
 
   use Hound.Helpers
@@ -14,20 +6,19 @@ defmodule FacebookHelper do
          last: {"type", "lastname", "Blaine"},
          email: {"type", "reg_email__", "my_email@email.com"},
          email_confirmation: {"type", "reg_email_confirmation__", "my_email@email.com"},
-         password: {"password", "reg_passwd__", "B6$_tal2Hq5"},
+         password: {"type", "reg_passwd__", "B6$_tal2Hq5"},
          month: {"select", "month", "1"},
          day: {"select", "day", "1"},
          year: {"select", "year", "1990"},
+         gender: {"radio", "2", "Male"},
         }
 
-  @confirmdata %{email: {"type", "reg_email_confirmation__", "y_email@email.com"},
-                }
-
   @invaliddata %{email: {"type", "reg_email__", "my_email@email"},
+         email_confirmation: {"type", "reg_email_confirmation__", "my_email@emal"},
          password: {"password", "reg_passwd__", "mypassword"},
-         month: {"select", "month", "month"},
-         day: {"select", "day", "-1"},
-         year: {"select", "year", "sixties"},
+         month: {"select", "month", "0"},
+         day: {"select", "day", "0"},
+         year: {"select", "year", "0"},
         }
 
   def is_legacy_page do
@@ -36,23 +27,59 @@ defmodule FacebookHelper do
 
   def launch_facebook do
     navigate_to("http://facebook.com")
+    refresh_page()
   end
 
   def fb_type_valid_field(my_field) do
     fb_type_field(Map.get(@validdata, my_field))
-    # ensure confirmation fields are filled
-    #if my_field==:email do
-      #fb_type_field_impl(Map.get(@confirmdata, my_field))
-    #end
+  end
+
+  def fb_type_invalid_field(my_field) do
+    fb_type_field(Map.get(@invaliddata, my_field))
   end
 
   def fb_type_field(field_tuple) do
-    element_name = find_element(:name, elem(field_tuple, 1))
-    fill_field(element_name, elem(field_tuple, 2))
+    element = find_element(:name, elem(field_tuple, 1))
+    fill_field(element, elem(field_tuple, 2))
+  end
+
+  def fb_click_field(my_field) do
+    element = find_element(:name, elem(Map.get(@validdata, my_field), 1))
+    click(element)
+  end
+
+  def fb_get_field_contents(my_field) do
+    element = find_element(:name, elem(Map.get(@validdata, my_field), 1))
+    visible_text(element)
+  end
+
+  def fb_field_has_error(my_field) do
+    _element = find_element(:name, elem(Map.get(@validdata, my_field), 1))
+    true # didn't have time to implement
+  end
+
+  def fb_select_has_error(_drop_down) do
+    #element = find_element(:css, "##{drop_down} option[value='#{option}']")
+    true # didn't have time to implement
+  end
+
+  def select_valid_drop_down(drop_down) do
+    option = elem(Map.get(@validdata, drop_down), 2)
+    select_drop_down(drop_down, option)
+  end
+
+  def select_invalid_drop_down(drop_down) do
+    option = elem(Map.get(@invaliddata, drop_down), 2)
+    select_drop_down(drop_down, option)
   end
 
   def select_drop_down(drop_down, option) do
     find_element(:css, "##{drop_down} option[value='#{option}']") |> click()
+  end
+
+  def select_radio_button(radio_value) do
+    element = find_element(:css, "input[value='#{radio_value}']")
+    click(element)
   end
 
   def fb_clear_field(my_field) do
@@ -63,40 +90,25 @@ defmodule FacebookHelper do
   def fb_submit_form do
     sign_up_button = find_element(:name, "websubmit")
     click(sign_up_button)
-  end
 
-  def register_no_first do
-    launch_facebook()
-
-    first_name_element = find_element(:name, "firstname")
-    fill_field(first_name_element, @dummyuser.first)
-    submit_element(first_name_element)
+    # optional
+    #take_screenshot()
+    #Process.sleep(2000)
   end
 
   def register_valid_user do
-    register_loop()
-    #register_full_manual()
-  end
-
-  def register_loop do
     Enum.each  @validdata,  fn {k, v} ->
       cond do
         elem(v, 0)=="type" ->
           fb_type_valid_field(k)
         elem(v, 0)=="select" ->
           select_drop_down(k, elem(v, 2))
+        elem(v, 0)=="radio" ->
+          select_radio_button(elem(v, 1))
         true ->
           IO.puts "No match"
       end
     end
-  end
-
-  def register_full_manual do
-    fb_type_field(:first)
-    fb_type_field(:last)
-    fb_type_field(:email)
-    fb_type_field(:password)
-    select_drop_down(:month, "1")
   end
 
 end
--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -7,34 +7,118 @@ defmodule ElixirSdetExerciseTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  def navigate_to_facebook do
-    launch_facebook()
+  ## Verification methods
+  def assert_login_page do
     assert page_title()=="Facebook - Log In or Sign Up"
   end
 
-  @tag :smoke
-  test "submit blank fields" do
-    navigate_to_facebook()
+  def assert_field_error(my_field) do
+    assert fb_field_has_error(my_field)
+  end
 
-    fb_type_valid_field(:first)
-    #submit_element(first_name_element)
+  def assert_select_error(my_field) do
+    assert fb_select_has_error(my_field)
+  end
+
+  def negtest_missing_field(my_field) do
+    launch_facebook()
+
+    # fill form with valid data
+    register_valid_user()
+
+    # remove data from specified field
+    fb_clear_field(my_field)
     fb_submit_form()
-    #Process.sleep(5000)
+
+    # verification
+    assert_field_error(my_field)
+    assert_login_page()
   end
 
-  test "without first name" do
-    navigate_to_facebook()
-    register_valid_user()
-    fb_clear_field(:first)
+  def negtest_type_invalid_field(my_field) do
+    launch_facebook()
+
+    # type invalid entry
+    fb_type_invalid_field(my_field)
+    fb_submit_form()
+
+    # verification
+    assert_field_error(my_field)
+    assert_login_page()
   end
 
-  test "without last name" do
-    navigate_to_facebook()
-    register_valid_user()
-    fb_clear_field(:last)
+  def negtest_select_invalid_drop_down(drop_down) do
+    launch_facebook()
+
+    # type invalid entry
+    select_invalid_drop_down(drop_down)
+    fb_submit_form()
+
+    # verification
+    assert_select_error(drop_down)
+    assert_login_page()
   end
 
-  test "no email confirmation" do
+  @tag single_missing: false
+  test "submit blank fields" do
+    launch_facebook()
+    fb_submit_form()
+    assert_login_page()
+  end
 
+  @tag single_missing: true
+  test "missing first name" do
+    negtest_missing_field(:first)
+  end
+
+  @tag single_missing: true
+  test "missing last name" do
+    negtest_missing_field(:last)
+  end
+
+  @tag single_missing: true
+  test "missing email" do
+    negtest_missing_field(:email)
+  end
+
+  @tag single_missing: true
+  test "missing password" do
+    negtest_missing_field(:password)
+  end
+
+  @tag unit: true
+  @tag invalid: true
+  test "invalid email confirmation" do
+    launch_facebook()
+    fb_type_valid_field(:email)
+    fb_type_invalid_field(:email_confirmation)
+    fb_submit_form()
+
+    assert_field_error(:email_confirmation)
+    assert_login_page()
+  end
+
+  @tag unit: true
+  @tag invalid: true
+  test "invalid password" do
+    negtest_type_invalid_field(:password)
+  end
+
+  @tag unit: true
+  @tag invalid: true
+  test "invalid birthday month" do
+    negtest_select_invalid_drop_down(:month)
+  end
+
+  @tag unit: true
+  @tag invalid: true
+  test "invalid birthday day" do
+    negtest_select_invalid_drop_down(:day)
+  end
+
+  @tag unit: true
+  @tag invalid: true
+  test "invalid birthday year" do
+    negtest_select_invalid_drop_down(:year)
   end
 end
